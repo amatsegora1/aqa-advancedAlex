06e7047c5d117e3e316f9eb302d4852b7c081194
amatsgora <you@example.com>
2025-02-25 22:51:56 +0200

    Chalk Homework

---
+++ nodeBasics/index.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+const log = console.log;
+
+log(chalk.blue('Hello Teacher!') + ' ' + chalk.red.bold('I have finished my Homework!') + `${chalk.yellow.bold(':)\n')}` + chalk.greenBright.underline.bold('Please check when you have time!'));
